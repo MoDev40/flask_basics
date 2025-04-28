d9abf874d79e0da8d25d0679117630c216b706be
M40 <modev.404@gmail.com>
2025-04-28 14:38:14 +0300

    welcome function

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 def syHi():
     return "Hi from flask"
 
-
+@app.route("/welcome")
+def welcome():
+    return "<h1>Welcome flask server</h1>"
 if __name__ == "__main__":
     app.run(debug=True)
