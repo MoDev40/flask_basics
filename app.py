a5430ddfeadcdb6b3744d07c96956aa017515464
M40 <modev.404@gmail.com>
2025-05-05 09:49:51 +0300

    insert and read

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
-from model import db
+from model import db,Person
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost/person"
 db.init_app(app)
 Migrate(db=db,app=app)
+
+def add():
+    new_person = Person(name="John doe")
+    db.session.add(new_person)
+    db.session.commit()
+
+def readAll():
+    data = Person.query.all()
+    return [ x.to_dis() for x in data ]
 
 with app.test_request_context():
     print('Hi testing...')
--- model.py
+++ model.py
@@ -5,3 +5,6 @@ db = SQLAlchemy()
 class Person(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(30))
+
+    def to_dis(self):
+        print(f' Hi id= {self.id} and name= {self.name}')
